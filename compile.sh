a2eed16ce5cdb5723f1d3c651f335034ea289ecd
Indy <Indy@debian.org>
2024-03-05 22:52:14 -0500

    cava added

--- compile.sh
+++ compile.sh
@@ -131,7 +131,7 @@ cd ~
 echo "##################"
 echo "## Yay Programs ##"
 echo "##################"
-yay -S vscodium-bin swaylock-effects rofi-lbonn-wayland-git brave-bin autojump hollywood swaync tty-clock
+yay -S cava vscodium-bin swaylock-effects rofi-lbonn-wayland-git brave-bin autojump hollywood swaync tty-clock
 
 echo "##################"
 echo "## Flatpak Repo ##"
